5b995ed52422539ed4c51eb82b52b47dcb19a2ab
vlw0052 <leon@vlw2.com>
2017-08-30 13:09:38 -0500

    Added protection of Dashboard route.

--- src/components/dashboards/OfficerDashboard.js
+++ src/components/dashboards/OfficerDashboard.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-
+import { withRouter } from 'react-router'
 class OfficerDashboard extends Component {
 	componentWillMount() {
-		console.log(this.props);
+		if(!this.props.officer)
+			this.props.history.push('/signin')
 	}
 	render() {
 		const { officer } = this.props
 		return (
-			<div><h2>Officer Dashboard - { officer.name }</h2></div>
+			<div><h2>Officer Dashboard - { officer && officer.name }</h2></div>
 		);
 	}
 }
@@ -17,4 +18,4 @@ const mapStateToProps = ({users}) => ({
 	officer:users.user,
 	loggedIn:users.loggedIn
 })
-export default connect(mapStateToProps)(OfficerDashboard)
+export default withRouter(connect(mapStateToProps)(OfficerDashboard))
