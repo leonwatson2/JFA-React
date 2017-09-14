19b401b856b0264e7fc5cc48ade8eb3847485077
vlw0052 <leon@vlw2.com>
2017-09-14 15:27:05 -0500

    Added jwt verify middleware and Response Status Code in utils file.

--- src/server/routes/events.js
+++ src/server/routes/events.js
@@ -1,67 +1,82 @@
 const router = require('express').Router()
 const handleError = require('../index').handleError
 const EventModel = require('../models/events').model
-	
-	/* /api/events
-	* GET: get all the events
-	* /api/events/:id get event by id
-	* POST: add an event to the database
-	* PUT: update an event in the database
-	* DELETE: delete an event from the database
-	* /api/events/:id delete events with ids
-	*/
+const jwt = require('jsonwebtoken')
+const config = require('../../../config.js')
+const responses = require('./utils.js').responses
 
-	router.get('/', (req, res)=>{
-		EventModel.find({}).exec((err, docs)=>{
-				res.status(200).json({docs})
-		})
+/* /api/events
+* GET: get all the events
+* /api/events/:id get event by id
+* POST: add an event to the database
+* PUT: update an event in the database
+* DELETE: delete an event from the database
+* /api/events/:id delete events with ids
+*/
+
+router.get('/', (req, res)=>{
+	EventModel.find({}).exec((err, docs)=>{
+			res.status(200).json({docs})
 	})
+})
 
-	router.post('/', (req, res)=>{
-		const event = req.body.event
-		if(event){
-			const newEvent = new EventModel(req.body.event);
-			newEvent.save()
-			res.status(200).json(newEvent)
-		}else{
-			handleError(res, {}, "You need to send an event object!")
-		}
-	})
+router.post('/', validToken, (req, res)=>{
+	const { event } = req.body
+	if(event){
+		const newEvent = new EventModel(req.body.event);
+		newEvent.save()
+		res.status(responses.CREATED).json(newEvent)
+	}else{
+		handleError(res, {}, "No event sent!", responses.BAD_REQUEST)
+	}
+})
 
-	router.put('/', (req, res)=>{
-		const { event } = req.body
-		console.log(event)
-		if(event){
-			const { _id } = event
-			if(_id){
-				delete event._id
-				EventModel.update({ _id }, event, (err, result)=>{
-					if(err){
-						handleError(res, err, "Updating the event didn't work!")
-					}else{
-						res.status(200).json({ ok:1 })
-					}
-				} )
-			}else{
-				res.status(400).json({ error:{ message:"No valid event id given."}})
-			}
-		}else{
-			res.status(400).json({error:"Huh", body:req.body})
-		}
-
-	})
-
-	router.delete('/:id', (req, res)=>{
-		const { id } = req.params
-		console.log(handleError);
-		
-		EventModel.deleteOne({_id:id}, (err, result)=>{
+router.put('/', (req, res)=>{
+	const { event } = req.body
+	if(event){
+		const { _id } = event
+		if(_id){
+			delete event._id
+			EventModel.update({ _id }, event, (err, result)=>{
 				if(err){
-					handleError(res, err, "Something went wrong deleting that.")
+					handleError(res, err, "Updating the event didn't work!", responses.SERVER_ERROR)
 				}else{
-					res.status(200).json({result})
+					res.sendStatus(204)
 				}
-			})
-	})
+			} )
+		}else{
+			res.status(responses.BAD_REQUEST).json({ error:{ message:"No valid event id given."}})
+		}
+	}else{
+		res.status(responses.BAD_REQUEST).json({error:"Huh", body:req.body})
+	}
+
+})
+
+router.delete('/:id', (req, res)=>{
+	const { id } = req.params
+	console.log(handleError);
+	
+	EventModel.deleteOne({_id:id}, (err, result)=>{
+			if(err){
+				handleError(res, err, "Something went wrong deleting that.")
+			}else{
+				res.sendStatus(responses.NO_CONTENT)
+			}
+		})
+})
 module.exports = router
 
+function validToken(req, res, next){
+	if(("authorization" in req.headers)){
+		const token = req.headers["authorization"].split(" ")[1]
+		req.token = token
+		jwt.verify(token, config.dbsecret, (err, data)=>{
+			if(err)
+				handleError(res, "Invalid Token", "Invalid Token")
+			next()
+		})
+	}else {
+		handleError(res, "No Auth token supplied", "No Auth token supplied")
+	}
+}
---
+++ src/server/routes/utils.js
@@ -0,0 +1,15 @@
+const responsesStatuses = {
+    OK:200,
+    CREATED:201,
+    ACCEPTED:202,
+    NO_CONTENT:204,
+    NOT_MODIFIED:304,
+    BAD_REQUEST:400,
+    FORBIDDEN:403,
+    NOT_FOUND:404,
+    SERVER_ERROR:500
+}
+
+module.exports = {
+    responses:responsesStatuses    
+}
