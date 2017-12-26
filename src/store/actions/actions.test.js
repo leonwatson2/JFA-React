e688f9b651443d64267ec9250be4b5dc8bba10d9
vlw0052 <leon@vlw2.com>
2017-12-25 20:25:22 -0600

    updated actions test for later testing

--- src/store/actions/actions.test.js
+++ src/store/actions/actions.test.js
@@ -2,23 +2,11 @@ import { getEvents, addEvent } from './eventActions'
 
 describe('event actions', ()=>{
 
-	it('should create an action that has a type GET_EVENTS', ()=>{
-		expect(getEvents()).toHaveProperty('type', 'GET_EVENTS')
-	})
-	it('should create an action that has a type ADD_EVENT', ()=>{
-		expect(addEvent()).toHaveProperty('type', 'ADD_EVENT')
-	})
-
-})
+	it('should call fetch')
+} )
 
 describe('user actions', ()=>{
-
-	// it('should create an action that has a type GET_EVENTS', ()=>{
-	// 	expect(getEvents()).toHaveProperty('type', 'GET_EVENTS')
-	// })
-	// it('should create an action that has a type ADD_EVENT', ()=>{
-	// 	expect(addEvent()).toHaveProperty('type', 'ADD_EVENT')
-	// })
-
-})
+	it('should call fetch')
+	
+} )
 
